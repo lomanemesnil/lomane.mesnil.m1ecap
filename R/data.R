829431b4d16f60d75bf503133658ba0ad3e270d0
lomanemesnil <lomane.mesnil@gmail.com>
2026-04-16 09:38:05 +0200

    explication des données

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' Résultats des marathons mondiaux
+#'
+#' Un jeu de données contenant tous les vainqueurs des marathons majors.
+#'
+#' @format ## `marathon2`
+#' A data frame with 536 rows and 7 columns:
+#' \describe{
+#'   \item{year}{Année de la course}
+#'   \item{winner}{Nom du vainqueur}
+#'   \item{gender}{Sexe (Male/Female)}
+#'   \item{country}{Pays d'origine}
+#'   \item{time}{Temps au format H:M:S}
+#'   \item{marathon}{Nom de la ville du marathon}
+#'   \item{time_seconds}{Temps total en secondes}
+#' }
+#' @source https://www.kaggle.com/datasets/emmanuelleai/world-marathons-majors
+"marathon2"
